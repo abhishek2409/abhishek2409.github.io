025a1be73614951dadfcccd993ac76ddfb520803
Abhishek Wadhawan <abhishek.wadhawan@ymail.com>
2018-03-23 15:03:55 +0530

    updates

--- src/components/containers/QuestionForm.js
+++ src/components/containers/QuestionForm.js
@@ -4,61 +4,109 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import uniqid from 'uniqid';
 
- class QuestionForm extends React.Component {
+class QuestionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      disabled:true
+      disabled: true,
+      errors: {
+        name: false,
+        phone: false,
+        comment: false,
+        email: false
+      }
     }
     this._onSubmit = this._onSubmit.bind(this);
+    this._onChange = this._onChange.bind(this);
   }
   componentDidMount() {
-    const disabled = this.checkValidations();
-    this.setState({disabled});
+    this.checkValidations();
   }
-  checkValidations(){
+  _onChange(e,type){
+    const {value} = e.target;
+    const {setValue} = this.props;
+    setValue(value,type)
+
+    this.checkValidations();
+  }
+
+  checkValidations() {
     let disabled = true;
-    const {name,email,phone,comment} = this.props.commentForm;
-    if((name != '') && (email != '') && (phone != '') && (comment != '')){
+    const {name, email, phone, comment} = this.props.commentForm;
+    if ((name != '') && (email != '') && (phone != '') && (comment != '')) {
       disabled = false;
     }
-    return disabled;
+    this.setState({disabled});
   }
-  _onSubmit(e){
+  _onSubmit(e) {
     e.preventDefault();
-    const {name,email,phone,comment,submitComment,selectedQuestion} = this.props.commentForm;
+    const {
+      name,
+      email,
+      phone,
+      comment,
+      submitComment,
+      selectedQuestion
+    } = this.props.commentForm;
     let params = {
       date_replied: new Date(),
       author: name,
       category: 'Category 2',
-      description:comment,
-      votes:0,
-      comments:0,
-      id:uniqid()
+      description: comment,
+      votes: 0,
+      comments: 0,
+      id: uniqid()
     }
-    submitComment(params,selectedQuestion.id);
+    submitComment(params, selectedQuestion.id);
   }
 
   render() {
-    const {disabled} = this.state;
+    const {disabled, errors} = this.state;
     const {commentForm} = this.props;
-    const {name,email,phone,comment} = commentForm;
+    const {name, email, phone, comment} = commentForm;
     return (<div className="form-wrapper">
       <form onSubmit={this._onSubmit}>
         <div className="form-row">
           <div className="form-group col-md-4">
-            <input type="text" className="form-control"  placeholder="Enter your name" required/>
+            <input type="text" className="form-control" onChange={e=>this._onChange(e,"name")} value={name} placeholder="Enter your name" required="required"/> {
+              errors.name
+                ? (<div class="invalid-feedback">
+                  {errors.name}
+                </div>)
+                : null
+            }
           </div>
           <div className="form-group col-md-4">
-            <input type="email" className="form-control" placeholder="Enter your email id" required/>
+            <input onChange={e=>this._onChange(e,"email")} type="email" className="form-control" value={email} placeholder="Enter your email id" required="required"/> {
+              errors.email
+                ? (<div class="invalid-feedback">
+                  {erros.email}
+                </div>)
+                : null
+            }
           </div>
           <div className="form-group col-md-4">
-            <input type="tel" className="form-control"  placeholder="09090909090" required/>
+            <input onChange={e=>this._onChange(e,"phone")} type="tel" className="form-control" placeholder="09090909090" value={phone} required="required"/> {
+              errors.phone
+                ? (<div class="invalid-feedback">
+                  {erros.phone}
+                </div>)
+                : null
+            }
           </div>
         </div>
-        <div className="form-group">
-          <textarea className="form-group" placeholder="Enter your comment" required/>
-            <small  className="form-text text-muted">50 of 200 characters left</small>
+        <div className="form-row">
+          <div className="form-group col-md-12">
+            <textarea className="form-control" onChange={e=>this._onChange(e,"comment")} placeholder="Enter your comment" required="required" value={comment} />
+            <small className="form-text text-muted float-right">{comment.length} of 200 characters left</small>
+            {
+              errors.comment
+                ? (<div class="invalid-feedback">
+                  {erros.comment}
+                </div>)
+                : null
+            }
+          </div>
         </div>
         <button type="submit" disabled={disabled} className="btn btn-primary">SUBMIT</button>
       </form>
@@ -67,15 +115,12 @@ import uniqid from 'uniqid';
 }
 
 function mapStateToProps(state) {
-  return {
-    commentForm:state.questions.commentForm,
-    selectedQuestion:state.questions.selectedQuestion
-  };
+  return {commentForm: state.questions.commentForm, selectedQuestion: state.questions.selectedQuestion};
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    submitComment:bindActionCreators(submitComment,dispatch)
+    submitComment: bindActionCreators(submitComment, dispatch)
   };
 }
 
